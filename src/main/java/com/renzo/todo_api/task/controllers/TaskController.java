495d19fa7aa8b7e9325d915458cf942a18b7125b
Renzo Pinto Quiroz <renzo25pinto@hotmail.com>
2026-06-30 21:34:33 -0500

    fix(logs): removed unnecessary logs

--- src/main/java/com/renzo/todo_api/task/controllers/TaskController.java
+++ src/main/java/com/renzo/todo_api/task/controllers/TaskController.java
@@ -5,7 +5,6 @@ import com.renzo.todo_api.task.dto.TaskResponse;
 import com.renzo.todo_api.task.models.TaskPriority;
 import com.renzo.todo_api.task.services.TaskService;
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +14,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-@Slf4j
 public class TaskController {
     private final TaskService taskService;
 
@@ -30,10 +28,10 @@ public class TaskController {
             @RequestParam(required = false) LocalDate dueBefore,
             @RequestParam(required = false) LocalDate dueAfter
     ) {
-        log.info("getAllTasks called");
         List<TaskResponse> tasks = taskService.getAllWithFilters(completed, priority, dueBefore, dueAfter);
         return ResponseEntity.status(HttpStatus.OK).body(tasks);
     }
+
 
     @PostMapping("")
     public ResponseEntity<TaskResponse> createTask(@Valid @RequestBody TaskRequest task) {
--- src/main/java/com/renzo/todo_api/task/services/TaskService.java
+++ src/main/java/com/renzo/todo_api/task/services/TaskService.java
@@ -6,14 +6,12 @@ import com.renzo.todo_api.task.mappers.TaskMapper;
 import com.renzo.todo_api.task.models.Task;
 import com.renzo.todo_api.task.models.TaskPriority;
 import com.renzo.todo_api.task.repositories.TaskRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
-@Slf4j
 public class TaskService {
     private final TaskRepository taskRepository;
     private final TaskMapper taskMapper;
@@ -29,7 +27,6 @@ public class TaskService {
             LocalDate dueBefore,
             LocalDate dueAfter
     ) {
-        log.info("getAllWithFilters called");
         List<Task> tasks = taskRepository.findAllWithFilters(completed, priority, dueBefore, dueAfter);
         return tasks.stream().map(taskMapper::toResponse).toList();
     }
